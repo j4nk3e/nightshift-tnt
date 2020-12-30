618e5861fde22e5aaf0c2ba4ddec6e0eaff59bf6
Julian Köpke <julian.koepke@gmail.com>
2020-12-30 20:07:52 +0100

    updated config

--- config.lua
+++ config.lua
@@ -1,15 +1,16 @@
 return {
-    database = 'host=/var/run/postgresql/ user=nightshift dbname=nightshift sslmode=disable',
+    database = 'host=/var/run/postgresql/ user=checkup dbname=checkup sslmode=disable',
+    api = {port = 8080},
     channels = {
         slack = os.getenv('SLACK_WEBHOOK')
     },
     monitors = {
-        api_health = {
+        prod_api = {
             type = 'http',
             interval_ms = 5000,
-            threshold_ms = 1500,
+            threshold_ms = 200,
             host = os.getenv('HOST'),
             alert = {'slack'}
-        }
+        },
     }
 }
