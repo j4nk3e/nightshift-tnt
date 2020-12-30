96f1e91c2569d6a6eb6e332db810361a230292aa
Julian Köpke <julian.koepke@gmail.com>
2020-12-30 20:07:02 +0100

    extracted http monitor class, added slack alerting

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 nightshift = require('nightshift')
-box.cfg {listen = 3311}
-nightshift.start(require 'config')
+box.cfg {listen = 3311, log_level = 6}
+nightshift.start(function() return require 'config' end)
